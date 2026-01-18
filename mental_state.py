5f2f5a87936da4b0672038b73cde1a58e1c36012
crunchys <105070975+crunchys@users.noreply.github.com>
2026-01-18 03:18:47 +0300

    Update mental_state.py

--- mental_state.py
+++ mental_state.py
@@ -6,10 +6,18 @@ from time import time
 class MentalState:
     arousal: float
     valence: float
+    dominance: float
     focus: Optional[str]
     timestamp: float
     existence_threat: float = 0.0
 
     @classmethod
     def initial(cls):
-        return cls(arousal=0.3, valence=0.0, focus=None, timestamp=time(), existence_threat=0.0)
+        return cls(
+            arousal=0.3, 
+            valence=0.0,
+            dominance=0.0,
+            focus=None, 
+            timestamp=time(), 
+            existence_threat=0.0
+        )
